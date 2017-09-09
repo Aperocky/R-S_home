1d57dfd12e3322367b52fb90cee48588e5794e90
Rocky Li <zl131@duke.edu>
2017-09-09 18:35:04 -0400

    test pic

--- activity.php
+++ activity.php
@@ -5,7 +5,17 @@
 
 <div class="grid">
   <div class="grid_container">
-    <p>1</p>
+    <div class="entry">
+      <img class="images" src="_images/pasta.jpg" alt="Pastafarian Church"/>
+      <div id="food-caption" class="caption_box">
+        <h3>FOOD</h3>
+        <p> We're a spicy couple and our favorite
+        food contains large amount of spices. We're also apex predators and
+        total carnivores and consumes prodigious amount of meat. Despite having
+        a great taste of food we accept wide ranges of culinary styles. Support
+        our food endeavors here, or bring us food!</p>
+      </div>
+    </div>
   </div>
   <div class="grid_container">
     <p>1</p>
