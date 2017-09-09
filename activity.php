1bc16776e2849394f769a9f80450b3b088af4335
Rocky Li <zl131@duke.edu>
2017-09-09 18:23:16 -0400

    Testing out grids

---
+++ activity.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php include_once('_pagepart/header.php'); ?>
+
+<link rel="stylesheet" type="text/css" href="_css/grid.css" />
+
+<div class="grid">
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+  <div class="grid_container">
+    <p>1</p>
+  </div>
+</div>
+
+<?php include_once('_pagepart/footer.php');
